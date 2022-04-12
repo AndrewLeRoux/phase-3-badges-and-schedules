241b9d66c0bac4885de0b2cae5da42737db7d722
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-04-12 10:46:18 -0700

    commit tree 8106c27ebdfaf73b042fb4675a63a11e5f104ed7

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    arr.map do |name| 
+        badge_maker(name)
+    end
+end
+
+
+def assign_rooms(arr)
+    arr.each_with_index do |name, index| 
+        arr[index] = "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+end
+
+def printer(arr)
+    batch_arr = batch_badge_creator(arr)
+    batch_arr.each do |statement|
+        puts statement
+    end
+    room = assign_rooms(arr)
+    room.each do |statement|
+        puts statement
+    end
+end
+
+p badge_maker("Arel")
+p batch_badge_creator(["Arel", "Carol"])
+p assign_rooms(["Arel", "Carol"])
